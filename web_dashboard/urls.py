1b2961ad3be267a0430d9a8f1807b9554bc73eb9
Denis Bolshakov <dreu007@gmail.com>
2024-02-19 22:26:43 +0400

    Update index.html urls.py

--- web_dashboard/urls.py
+++ web_dashboard/urls.py
@@ -15,17 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path(
-        'login/',
-        LoginView.as_view(template_name='users/login.html', next_page='index'),
-        name='login'
-    ),
+    path('', include('web_dashboard.auth.urls')),
     path('admin/', admin.site.urls),
 ]
