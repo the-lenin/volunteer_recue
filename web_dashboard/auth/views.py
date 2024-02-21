359350f9ef2238f827d34d6b1cb507da92694296
Denis Bolshakov <dreu007@gmail.com>
2024-02-21 14:43:06 +0400

    Update views.py: custom form

--- web_dashboard/auth/views.py
+++ web_dashboard/auth/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from . import forms
 
 
 class CustomLoginView(LoginView):
-    """Context extended."""
+    """Assign arguments for LoginView."""
     template_name = 'auth/login.html'
     next_page = reverse_lazy('index')
     user_agreement_fp = 'templates/auth/user_agreement.md'
+
+    form_class = forms.CustomLoginForm
+
+    # def form_valid(self, form):
+    #     remember_me = form.cleaned_data['remember_me']
+    #     if not remember_me:
+    #         self.request.session.set_expiry(0)
+    #         self.request.session.modified = True
+    #     return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         """Add context to super()."""
