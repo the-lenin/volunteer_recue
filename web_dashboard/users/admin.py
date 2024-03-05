8cce109250177ba1db77087b11fa82dc88eab563
Denis Bolshakov <dreu007@gmail.com>
2024-03-05 11:10:11 +0400

    Update admin.py: CustomUserAdmin settings

--- web_dashboard/users/admin.py
+++ web_dashboard/users/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+# from .forms import CustomUserCreationForm
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+#    add_form = CustomUserCreationForm
+
+    list_display = (
+         'username',
+         'full_name',
+         'email',
+         'phone_number',
+    )
+
+    list_filter = (
+        'has_car',
+        'groups'
+    )
+
+    fieldsets = (
+        (_('Details'), {
+            "fields": (
+                "password",
+                'nickname',
+                "first_name",
+                "last_name",
+                "patronymic_name",
+                "email",
+                "phone_number",
+                "address",
+                'comment'
+             )
+        }),
+        (_("Permissions"), {
+            "fields": (
+                "groups",
+                "user_permissions",
+                "is_staff",
+                "is_active",
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        (_('Details'), {
+            "fields": (
+                'username',
+                "password",
+                'nickname',
+                "first_name",
+                "last_name",
+                "patronymic_name",
+                "email",
+                "phone_number",
+                "address",
+                'comment'
+             )
+        }),
+        (_("Permissions"), {
+            "fields": (
+                "groups",
+                "user_permissions",
+                "is_staff",
+                "is_active",
+            )
+        }),
+    )
+
+    search_fields = (
+        'username',
+        'first_name',
+        'last_name',
+        'patronymic_name',
+        'nickname',
+        'email',
+        'phone_number',
+        'address',
+        'comment',
+    )
+
+    ordering = (
+        'email',
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(CustomUser, CustomUserAdmin)
