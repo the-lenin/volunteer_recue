0f3ef3026bb25cfc5f23ff2d4d8c1854818d00ee
Denis Bolshakov <dreu007@gmail.com>
2024-03-09 22:01:39 +0400

    Update views.py: SearchRequest CRUD

--- web_dashboard/search_requests/views.py
+++ web_dashboard/search_requests/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import SearchRequest
 
-# Create your views here.
+
+class SearchRequestView(View):
+    """Base view for a SearchRequest."""
+    model = SearchRequest
+    fields = '__all__'
+    success_url = reverse_lazy('search_request:all')
+
+
+class SearchRequestListView(SearchRequestView, ListView):
+    """List all SearchRequests view."""
+
+
+class SearchResquestCreateView(SearchRequestView, CreateView):
+    """SearchRequest create view."""
+
+
+class SearchRequestDetailView(SearchRequestView, DetailView):
+    """SearchRequest detail view."""
+
+
+class SearchRequestUpdateView(SearchRequestView, UpdateView):
+    """SearchRequest update view."""
+
+
+class SearchRequestDeleteView(SearchRequestView, DeleteView):
+    """SearchRequest delete view."""
