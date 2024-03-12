8b1862090ba5a176d6e04f4782574188924042db
Denis Bolshakov <dreu007@gmail.com>
2024-03-12 17:07:22 +0400

    Update settings.py: static

--- web_dashboard/settings.py
+++ web_dashboard/settings.py
@@ -195,8 +195,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
