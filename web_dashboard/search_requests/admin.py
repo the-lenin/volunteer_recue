13beb7ec39b8b49773dfbbe76a898bbfe850f81a
Denis Bolshakov <dreu007@gmail.com>
2024-03-15 14:14:44 +0400

    Update admin.py: map widget

--- web_dashboard/search_requests/admin.py
+++ web_dashboard/search_requests/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis import admin as admin_gis
 from . import models
 
 
-class SearchRequestAdmin(admin.ModelAdmin):
+class SearchRequestAdmin(admin_gis.GISModelAdmin):
     """Defines the look of the model in admin dashboard panel."""
     list_display = ('full_name', 'date_of_birth', 'status', 'location')
 
 
-admin.site.register(models.SearchRequest,)
+admin.site.register(models.SearchRequest, admin_gis.GISModelAdmin)
 # SearchRequestAdmin)
 
 admin.site.register(models.Reporter,
