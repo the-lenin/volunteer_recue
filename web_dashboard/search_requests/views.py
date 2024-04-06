acf42acda6f9023056a13fc4c4baa8a5f906b67d
Denis Bolshakov <dreu007@gmail.com>
2024-04-06 11:39:17 +0400

    Update views.py: Survey create view

--- web_dashboard/search_requests/views.py
+++ web_dashboard/search_requests/views.py
@@ -1,13 +1,15 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from . import models
 from django.utils.translation import gettext_lazy as _
 
 
+# Search Request
 class SerRequestBaseView(SuccessMessageMixin, View):
     """Base view for a SearchRequest."""
     model = models.SearchRequest
@@ -51,16 +53,42 @@ class SearchRequestDeleteView(SerRequestBaseView, DeleteView):
     success_message = _('Request succussfully deleted')
 
 
+# Survey
 class SurveyBaseView(View):
     """Base view for a Survey."""
     model = models.Survey
     fields = '__all__'
     context_object_name = "survey"
 
+    def get_success_url(self):
+        """Return to detailed view of search_request."""
+        return reverse('search_requests:read', self.search_requests.id)
+
 
 class SurveyCreateView(SurveyBaseView, CreateView):
     """Survey create view."""
     success_message = _('Survey succussfully created')
+
+    # @overide
+    def post(self, request, *args, **kwargs):
+        """Get SearchRequest primary key and assign it to Survey."""
+        form = self.get_form()
+        if form.is_valid():
+            survey = form.save()
+            search_request = models.SearchRequest.objects.get(
+                pk=self.kwargs.get('pk')
+            )
+
+            if search_request:
+                print(search_request.id, survey.id)
+                models.SurveySearchRequest.objects.create(survey=survey,
+                                                          search_request=search_request)
+                return redirect(search_request.get_absolute_url())
+
+            # TODO: Where to return if no search_request present???
+            return redirect('search_requests:all')
+
+        return self.form_invalid(form)
 
 
 class SurveyDetailView(SurveyBaseView, DetailView):
