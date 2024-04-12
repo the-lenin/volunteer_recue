7fe3cb4c4c6eef6f92f212bcf4376d7ddb5f6e37
Denis Bolshakov <dreu007@gmail.com>
2024-04-12 16:08:44 +0100

    Update admin.py: register logisics models

--- web_dashboard/logistics/admin.py
+++ web_dashboard/logistics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Crew)
+admin.site.register(models.Task)
+admin.site.register(models.Departure)
