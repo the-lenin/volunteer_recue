5abd04e6c64f5aa8c8cba26822a3580c7cf8f78a
Denis Bolshakov <dreu007@gmail.com>
2024-04-12 16:08:06 +0100

    Update apps.py: app name

--- web_dashboard/logistics/apps.py
+++ web_dashboard/logistics/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LogisticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'logistics'
+    name = 'web_dashboard.logistics'
