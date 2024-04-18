4d911c0c2f6da3cc2e4a4a0a05248c9ff76c9295
Denis Bolshakov <dreu007@gmail.com>
2024-04-18 12:44:40 +0100

    Update filters.py: Departure filter

--- web_dashboard/logistics/filters.py
+++ web_dashboard/logistics/filters.py
@@ -30,3 +30,30 @@ class CrewFilter(django_filters.FilterSet):
     #         )
 
     #     return queryset
+
+
+class DepartureFilter(django_filters.FilterSet):
+    """Set of filters for the Departure model."""
+    # search = django_filters.CharFilter(
+    #     label=_('Search'),
+    #     label_suffix="",
+    #     method='filter_all'
+    # )
+
+    class Meta:
+        model = models.Departure
+        fields = [
+            # 'search',
+            'status',
+        ]
+
+    # def filter_all(self, queryset, name, value):
+    #     """Filter Departure queryset for multiple lookups."""
+    #     if value:
+    #         return queryset.filter(
+    #             Q(full_name__icontains=value) |
+    #             Q(city__icontains=value) |
+    #             Q(phone_number__icontains=value)
+    #         )
+
+    #     return queryset
