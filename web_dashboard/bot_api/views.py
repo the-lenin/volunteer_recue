6fb32e91e33bca5383649bd2b2b5c9b412c6edd3
Denis Bolshakov <dreu007@gmail.com>
2024-05-04 21:22:34 +0100

    Update views.py: get_open_departures method

--- web_dashboard/bot_api/views.py
+++ web_dashboard/bot_api/views.py
@@ -3,12 +3,14 @@ import json
 from functools import wraps
 from django.views import View
 from django.conf import settings
+from django.core import serializers
 from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.utils.translation import gettext as _
 from secrets import compare_digest
 
 from web_dashboard.search_requests.models import SearchRequest
 from web_dashboard.logistics.models import Departure
+from web_dashboard.logistics.serializers import DepartureSerializer
 
 
 class AuthTokenMixinView(View):
@@ -53,6 +55,9 @@ class WebhookView(AuthTokenMixinView):
             case 'info':
                 return self.get_info(payload)
 
+            case 'get_open_departures':
+                return self.get_open_departures(payload)
+
     def get_info(self, payload: dict) -> JsonResponse:
         """Return present quantity of open SearchRequests and Departures."""
         search_requests = SearchRequest.objects.filter(
@@ -68,4 +73,20 @@ class WebhookView(AuthTokenMixinView):
         )
         return JsonResponse({'msg': msg})
 
+    def get_open_departures(self, payload):
+        open_departures = Departure.objects.filter(
+            status=Departure.StatusVerbose.OPEN
+        ).prefetch_related('search_request')
 
+        departures = [
+            DepartureSerializer(dep).data for dep in open_departures
+        ]
+
+        json_response = {
+            'msg': 'Ok, received',
+            'departures': departures,
+        }
+        print(departures)
+        # data = serializers.serialize('json', json_response)
+        # print(data)
+        return JsonResponse(json_response)
