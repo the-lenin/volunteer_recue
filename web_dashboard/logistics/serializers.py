231b8cd45a20d043e4a18e3a17aac0ebffbce4ba
Denis Bolshakov <dreu007@gmail.com>
2024-05-05 10:02:33 +0100

    Refactor serializers.py

--- web_dashboard/logistics/serializers.py
+++ web_dashboard/logistics/serializers.py
@@ -1,17 +1,9 @@
 from rest_framework import serializers
 from . import models
 
-from web_dashboard.search_requests.serializers import SearchRequestSerializer
-
 
 class DepartureSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Departure
         depth = 1
         fields = '__all__'
-
-#    def to_representation(self, instance):
-#        representation = super().to_representation(instance)
-#        representation['search_request'] = SearchRequestSerializer(instance.search_request).data
-##         representation['crew'] = CrewSerializer(instance.crew).data
-#        return representation
