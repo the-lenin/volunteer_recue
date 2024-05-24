068901c1aabda3fa00c42be958b01e0f6948ae22
Denis Bolshakov <dreu007@gmail.com>
2024-05-24 14:58:58 +0100

    Update admin.py models.py: add TelegramUser model

--- web_dashboard/bot_api/models.py
+++ web_dashboard/bot_api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    """
+    Represents a Telegram User to track actions and activities of the user.
+    """
+    user_id = models.BigIntegerField(
+        primary_key=True,
+        verbose_name=_('User ID'),
+    )
+
+    last_action = models.DateTimeField(
+        _('Last action'),
+    )
+
+    created_at = models.DateTimeField(
+        _('Created at'),
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        _('Updated at'),
+        auto_now=True
+    )
